415efa185768fcdb1d9ee6fefa8f4fe9248fce11
siteknower <office@siteknower.com>
2024-10-20 14:59:59 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace StnwSampleCS02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dsCustomers = new System.Data.DataSet();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dsCustomers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dsCustomers
+            // 
+            this.dsCustomers.DataSetName = "dsCustomers";
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Location = new System.Drawing.Point(23, 26);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.Size = new System.Drawing.Size(444, 133);
+            this.DataGridView1.TabIndex = 1;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(392, 199);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 23);
+            this.Button1.TabIndex = 81;
+            this.Button1.Text = "Print";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 261);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.DataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dsCustomers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.DataSet dsCustomers;
+        internal System.Windows.Forms.DataGridView DataGridView1;
+        internal System.Windows.Forms.Button Button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StnwService;
+
+namespace StnwSampleCS02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string filename = "schema.xml";
+
+            System.IO.FileStream stream =
+                new System.IO.FileStream(filename, System.IO.FileMode.Open);
+
+            dsCustomers.ReadXmlSchema(stream);
+
+            DataSet tds = GetData();
+
+            DataGridView1.DataSource = tds.Tables["Users"];
+
+            foreach (DataGridViewColumn column in DataGridView1.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private DataSet GetData()
+        {
+            DataTable dt = dsCustomers.Tables["Users"];
+            dt.Rows.Add("ABDEN", "Maria Weiss", "Berlin", "Germany");
+            dt.Rows.Add("AXEIS", "Pedro Alvarez", "México D.F.", "Mexico");
+            dt.Rows.Add("BENOI", "Anna Tóth", "Szeged", "Hungary");
+            dt.Rows.Add("CAZLE", "Jan Eriksson", "Mannheim", "Sweden");
+            dt.Rows.Add("DRFIS", "Giulia Donatelli", "Milano", "Italia");
+            return dsCustomers;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            clsStnwClass tsi = new clsStnwClass();
+            string preslAccountCode = "DEMO1";  // your account code
+            string preslUserCode = "0000"; // yout user code
+
+            tsi.preslAccountCode = preslAccountCode;
+            tsi.preslUserCode = preslUserCode;
+            tsi.dsRPT = dsCustomers;
+            tsi.RPTDEST = 0;
+
+            string rptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CustomerReport1.rpt");
+            tsi.ReportFullName = rptPath;
+
+            tsi.ShowForm();
+        }
+    }
+}
